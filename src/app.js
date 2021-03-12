7857db604c789117eac4dacb3cc4cce65e562625
rysamar <ysalcantara.v@gmail.com>
2021-03-12 12:22:01 +0000

    excuse generator con js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generator() {
+  let theExcuse = "";
+  let who = ["Me ", "My mom ", "My dad ", "My friend "];
+  let did = ["jump ", "sing ", "cook ", "walk "];
+  let what = ["my sandwich ", "my computer ", "my phone ", "my face "];
+  let when = ["today", "yesterday", "an hour ago", "a year ago"];
+
+  let number = Math.floor(Math.random() * 4);
+
+  theExcuse =
+    theExcuse + who[number] + did[number] + what[number] + when[number];
+
+  return theExcuse;
+}
+document.write(generator());
